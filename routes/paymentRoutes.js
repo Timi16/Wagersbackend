9dbdf350c67b177807de64ca9fda3938080490cc
Timi16 <timilehinolowu46@gmail.com>
2025-06-28 03:50:19 -0700

    Adding banks and withdrawal

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 // paymentRoutes.js
 import express from 'express';
-import { withdraw } from '../controllers/paymentController.js';
+import { getBanks, withdraw } from '../controllers/paymentController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
